7f2e444c1e6f210f793beeba816120d55fcf0e4f
Yuki <>
2023-06-02 04:04:58 +0900

    commit

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import os
 
 import discord
 from discord import *
+from discord.app_commands import *
+from discord.ext.commands import *
+from discord.ext import *
+
+from selfintrod import selfintrod
 
 intents = Intents.all()
 
-bot = discord.Client(intents=intents)
+bot = Bot(intents=intents, command_prefix=None)
+
+# bot.add_cog(selfintrod(bot))
 
 @bot.event
 async def on_ready():
     print(f"Logged in as: {bot.user}")
+    await bot.load_extension("selfintrod")
 
 bot.run(os.environ.get("DISCORD_TOKEN"))
